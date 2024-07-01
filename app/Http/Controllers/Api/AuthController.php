cac860fe338bbe96b5c32596703ebab68b25912a
Ivor Szaniszló <szaniszlo.ivor@gmail.com>
2024-07-01 14:08:32 +0200

    ADD: Tokens and Signing Out

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -39,10 +39,10 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
-        auth()->user()->tokens()->delete();
+        $request->user()->tokens()->delete();
 
         return response()->json([
-            'message' => 'Logged out'
+            'message' => 'Logged out successfully.'
         ]);
     }   
 }
