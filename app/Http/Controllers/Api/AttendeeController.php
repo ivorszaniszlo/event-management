8a656eb541a59511b51ab21faf09ea34b948d07d
Ivor Szaniszló <szaniszlo.ivor@gmail.com>
2024-07-02 23:08:29 +0200

    ADD: Throttling the API

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -18,6 +18,8 @@ class AttendeeController extends Controller
     public function __construct()
     {
         $this->middleware('auth:sanctum')->except(['index', 'show', 'update']);
+        $this->middleware('throttle:api')
+            ->only(['store', 'destroy']);
         $this->authorizeResource(Attendee::class, 'attendee');
     }
     public function index(Event $event)
@@ -39,7 +41,7 @@ class AttendeeController extends Controller
     {
         $attendee = $this->loadRelationships(
             $event->attendees()->create([
-                'user_id' => 1
+                'user_id' => $request->user()->id
             ])
         );
 
